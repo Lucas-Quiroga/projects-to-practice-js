9fd197c7ea95d41953e40c56e408f3acfdbc2b45
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2022-06-03 00:02:46 -0300

    un poco de css y arreglando bugs

--- app  - weather app/main.js
+++ app  - weather app/main.js
@@ -5,36 +5,36 @@ const btn=document.getElementById("buttonSearch");
 const sectionAjax=document.getElementById("conteinerAjax");
 const ul=document.getElementById("cities");
 
-
-
+//api key
 const apiKey="16391637de1f7272867ebaddb29be797";
 
-
+//formulario con evento submit que trae la api del clima con sus respectivos datos
 form.addEventListener("submit", e=>{
+    //para que no recargue la pagina
     e.preventDefault();
+    //guardamos el dato que escribe el usuario
     const inputValue=input.value;
 
-    //ajax
+    //aplicamos en la url el value del input + la keyApi
     const apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}`
     fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
       const { main, name, sys, weather } = data;
-    
-        const creatingLi=document.createElement("li")
-        creatingLi.innerHTML=`${name}`
-        sectionAjax.appendChild(creatingLi);
+      //guardamos el icono en una constante para volver a usarlo
+      const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+        //creamos las cards
+        const createCard=document.createElement("div");
+        createCard.setAttribute("class","card");
+        createCard.style.width="18rem";
+        createCard.innerHTML=`<img src="${icon}" class="card-img-top" alt="${weather[0]["main"]}">
+                              <div class="card-body">
+                              <h5 class="card-title">${name} - ${sys.country}</h5>
+                              <p class="card-text">Temperatura ${Math.round(main.temp_min)}<sup>°C</sup></p>
+                              <figcaption>${weather[0]["description"]}</figcaption>
+                              </div>`
+        sectionAjax.appendChild(createCard);
     });
+    form.reset();
+    input.focus();
 })
-    // li.setAttribute("class","city");
-    // li.innerHTML= `<h2 class="city-name" data-name="${name},${sys.country}"><span>${name}</span><sup>${sys.country}</sup></h2>
-    // <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
-    // <figure>
-    // <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
-    // <figcaption>${weather[0]["description"]}</figcaption></figure>`;;
-    // sectionAjax.appendChild(li);
-    // 
-
-/*links 
-https://webdesign.tutsplus.com/es/tutorials/build-a-simple-weather-app-with-vanilla-javascript--cms-33893
-https://openweathermap.org/api*/
